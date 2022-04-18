4a3043adf4bbb6c28a02c399abfd49ac9b19eb35
yash somalkar <Yashsomalkar>
2022-04-19 04:51:47 +0530

    conected&working firebase :)

--- Chatlin/app/src/main/java/com/yashacker/chatlin/Login_Activity.kt
+++ Chatlin/app/src/main/java/com/yashacker/chatlin/Login_Activity.kt
@@ -1,12 +1,13 @@
 package com.yashacker.chatlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.util.Log
+import android.util.Patterns
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class Login_Activity : AppCompatActivity() {
     private lateinit var  editEmail: EditText
@@ -14,17 +15,81 @@ class Login_Activity : AppCompatActivity() {
     private lateinit var btnlogin : Button
     private lateinit var textsignup : TextView
     private lateinit var loginbacktowelcome : ImageView
+    private lateinit var mAuth : FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance()
         loginbacktowelcome = findViewById(R.id.loginbacktowelcome)
+        editEmail = findViewById(R.id.editEmail)
+        editPassword = findViewById(R.id.editPassword)
+        btnlogin = findViewById(R.id.btnlogin)
+        textsignup = findViewById(R.id.textsignup)
+
+
         loginbacktowelcome.setOnClickListener {
             finish()
         }
+        btnlogin.setOnClickListener{
+
+            credential_check()
+        }
+        textsignup.setOnClickListener{
+            val intent = Intent(this@Login_Activity, SignUp_Activity::class.java)
+            startActivity(intent)
+        }
+
+    }
+    private fun login(email: String, password: String){
+        //logic for logging in user
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val intent = Intent(this@Login_Activity, MainActivity::class.java)
+                    startActivity(intent)
 
 
 
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(this@Login_Activity, "User does not Exist!",
+                        Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+    }
+    private fun credential_check() {
+        val email = editEmail.text.toString()
+        val password = editPassword.text.toString()
+        val emailAddress: String = email.trim()
+        if (password.length < 6) {
+            editPassword.error = "password minimum contain 6 character"
+            editPassword.requestFocus()
+        }
+        if (password == "") {
+            editPassword.error = "please enter password 🤣"
+            editPassword.requestFocus()
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()) {
+            editEmail.error = "please enter valid email address"
+            editEmail.requestFocus()
+        }
+        if (email == "") {
+            editEmail.error = "please enter email address 🤣"
+            editEmail.requestFocus()
+        }
+        if (emailAddress != "" && password.length >= 6 &&
+            password.trim() != "" &&
+            Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()
+        ) {
+
+            login(email, password)
+        }
     }
 }
--- Chatlin/app/src/main/java/com/yashacker/chatlin/SignUp_Activity.kt
+++ Chatlin/app/src/main/java/com/yashacker/chatlin/SignUp_Activity.kt
@@ -1,18 +1,98 @@
 package com.yashacker.chatlin
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.util.Log
+import android.util.Patterns
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp_Activity : AppCompatActivity() {
     private lateinit var signupbacktowelcome : ImageView
+    private lateinit var textlogin : TextView
+    private lateinit var  editEmail: EditText
+    private lateinit var  editUsername: EditText
+    private lateinit var  editPassword : EditText
+    private lateinit var btnsignup : Button
+    private lateinit var mAuth : FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         signupbacktowelcome = findViewById(R.id.signupbacktowelcome)
+        editEmail = findViewById(R.id.editEmail)
+        editPassword = findViewById(R.id.editPassword)
+        btnsignup = findViewById(R.id.btnsignup)
+        textlogin = findViewById(R.id.textlogin)
+        editUsername = findViewById(R.id.editUsername)
+        mAuth = FirebaseAuth.getInstance()
+
         signupbacktowelcome.setOnClickListener {
             finish()
+        }
+        btnsignup.setOnClickListener {
+            credential_check()
+
+
+        }
+        textlogin.setOnClickListener{
+            val intent = Intent(this@SignUp_Activity, Login_Activity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun signUp(email: String, password: String){
+        //logic for creating user
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "createUserWithEmail:success")
+
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this@SignUp_Activity, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(this@SignUp_Activity, "Authentication failed.", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+    private fun credential_check() {
+        val email = editEmail.text.toString()
+        val password = editPassword.text.toString()
+        val username = editUsername.text.toString()
+        val emailAddress: String = email.trim()
+        if (password.length < 6) {
+            editPassword.error = "password minimum contain 6 character"
+            editPassword.requestFocus()
+        }
+        if (password == "") {
+            editPassword.error = "please enter password 🤣"
+            editPassword.requestFocus()
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()) {
+            editEmail.error = "please enter valid email address"
+            editEmail.requestFocus()
+        }
+        if (email == "") {
+            editEmail.error = "please enter email address 🤣"
+            editEmail.requestFocus()
+        }
+        if (username == "") {
+            editUsername.error = "please enter Username 😤"
+            editUsername.requestFocus()
+        }
+        if (emailAddress != "" && password.length >= 6 &&
+            password.trim() != "" &&
+            Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()
+        ) {
+
+            signUp(email, password)
         }
     }
 }

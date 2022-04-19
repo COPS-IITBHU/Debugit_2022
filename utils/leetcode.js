76e5e2658dddbb9f3281b2d37b914e4d9bf0efc4
Adrito-M <adrito.mukherjee.cse21@itbhu.ac.in>
2022-04-19 21:18:06 +0530

    leetcode added

---
+++ utils/leetcode.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+
+module.exports = {
+    getCodeforcesData : async function () {
+
+        const contests = await axios.get('https://leetcode.com/graphql?query={%20upcomingContests%20{%20title%20titleSlug%20startTime%20duration%20}%20}').then(obj => obj.data.data.upcomingContests)
+        let contestArr = []
+        for (let contest of contests) {
+
+            let tempObj = {
+                website: 'leetcode',
+                contestName: contest.title,
+                contestLink: `https://leetcode.com/contest/${contest.titleSlug}`,
+                contestStartTime: new Date(contest.startTime * 1000),
+                contestDurationInMins: Math.floor(contest.duration / 60),
+            }
+
+            contestArr.push(tempObj)
+        }
+        return contestArr
+    }
+}
